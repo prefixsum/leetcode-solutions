db0423ac57d7f9a42551df50ed0ce238862532b4
prefixsum <prefixsum@outlook.com>
2024-06-04 19:21:16 +0100

    Tweak 1. Two Sum in rust

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,4 @@
 #![allow(dead_code)]
+#![allow(unused_doc_comments)]
 
 mod solutions;
--- rust/src/solutions/_0001_two_sum.rs
+++ rust/src/solutions/_0001_two_sum.rs
@@ -4,26 +4,48 @@ use std::collections::HashMap;
 
 impl Solution {
     fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        // Initialise dictionary of seen element
+        // Initialise map of seen elements
         let mut seen: HashMap<i32, i32> = HashMap::new();
         // Pass over inputs
         for (i, &n) in nums.iter().enumerate() {
             // Get complement
             let complement: i32 = target - n;
-            if let Some(&j) = seen.get(&complement) {
-                return vec![j, i as i32];
-            } else {
-                seen.insert(n, i as i32);
+            // Check if current element's complement has been seen
+            if let Some(&complement_index) = seen.get(&complement) {
+                // Return current index and the complement's index
+                return vec![complement_index, i as i32];
             }
+            // Add current input to map
+            seen.insert(n, i as i32);
         }
+        // Return empty vector if no pair found
         vec![]
     }
 }
 
 #[test]
 fn test() {
+    /// Given test case from Leetcode.
     let case = vec![2, 7, 11, 15];
     let target = 9;
+    let expected = vec![0, 1];
+    assert_eq!(Solution::two_sum(case, target), expected);
+}
+
+#[test]
+fn test_last() {
+    /// Given test case from Leetcode, with one index being the final index.
+    let case = vec![3, 2, 4];
+    let target = 6;
+    let expected = vec![1, 2];
+    assert_eq!(Solution::two_sum(case, target), expected);
+}
+
+#[test]
+fn test_repeated() {
+    /// Given test case from Leetcode, with repeated values.
+    let case = vec![3, 3];
+    let target = 6;
     let expected = vec![0, 1];
     assert_eq!(Solution::two_sum(case, target), expected);
 }
