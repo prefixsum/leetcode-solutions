f5073fa9b131771e7dc358b8ee8db29031757469
prefixsum <prefixsum@outlook.com>
2024-06-07 15:17:45 +0100

    Optimise 648. Replace Words with trie

--- python/solutions/0648-replace-words.py
+++ python/solutions/0648-replace-words.py
@@ -17,6 +17,52 @@ from typing import Dict, List, Optional, Set, Tuple
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
         """
+        Turn the dictionary into a trie of roots. For each word in the sentence, check
+        if each root (i.e., each substring starting at 0) is in the trie, terminating
+        early if a match is not possible. If the root exists, replace the word with it,
+        and then continue to the next word.
+        """
+        # Turn dictionary into trie of roots
+        roots = {}
+        for root in dictionary:
+            current = roots
+            for i in range(len(root)):
+                c = root[i]
+                # Handle current root is redundant
+                if c in current and not current[c]:
+                    break
+                # Handle previous root is now redundant
+                if i == len(root) - 1:
+                    current[c] = {}
+                # Initialise node if it does not exist
+                if c not in current:
+                    current[c] = {}
+                # Move to next node
+                current = current[c]
+        # Iterate over words in sentence
+        words = sentence.split(" ")
+        for i in range(len(words)):
+            # Get current word
+            word = words[i]
+            # Find shortest root
+            shortest_root = ""
+            current = roots
+            for c in word:
+                shortest_root += c
+                # Case of no root; terminate early
+                if c not in current:
+                    break
+                # Case of root found; replace word
+                if not current[c]:
+                    words[i] = shortest_root
+                    break
+                # Case of still searching for root
+                current = current[c]
+        # Reform words in sentence and return
+        return " ".join(words)
+
+    def replaceWordsSet(self, dictionary: List[str], sentence: str) -> str:
+        """
         Turn the dictionary into a set. For each word in the sentence, check if each
         root (i.e., each substring starting at 0) is in the set. If so, replace the word
         with the root, and then continue to the next word.
